53abcb3ac8f915b9ec83b1816628d608ed198356
acanhost <acanstudio@canliang.wang>
2022-08-26 08:59:58 +0800

    develop website

--- resources/views/classical/modules/_jscss.blade.php
+++ resources/views/classical/modules/_jscss.blade.php
@@ -1,5 +1,5 @@
 <meta http-equiv="imagetoolbar" content="no">
-<link rel="stylesheet" href="{{$commonAssetUrl}}/classical/css/d817a-6122.css" type="text/css">
+<link rel="stylesheet" href="{{$commonAssetUrl}}/classical/css/d817a-6122.css?v=1" type="text/css">
 @switch($view)
 @case('detail')
 <link rel="stylesheet" href="{{$commonAssetUrl}}/classical/css/dd99b-8811.css?v=47" type="text/css">
@@ -8,7 +8,7 @@
 <script type="text/javascript" src="{{$commonAssetUrl}}/classical/js/common_new.js?v=7"></script>
 @break
 @default
-<link rel="stylesheet" href="{{$commonAssetUrl}}/classical/css/3ade8-3930.css?v=36" type="text/css">
+<link rel="stylesheet" href="{{$commonAssetUrl}}/classical/css/3ade8-3930.css?v=38" type="text/css">
 <script type="text/javascript" src="{{$commonAssetUrl}}/classical/js/index_new.js?v=1"></script>
 <style>.piny{font-family:Arial,Verdana,Sans-serif;}</style>
 @endswitch
